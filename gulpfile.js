db88d6cbecfad5104c4a340deef37e4c4d95c843
Alisher <alisher.e.tagaev@gmail.com>
2021-05-27 16:02:11 +0500

    change

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+function defaultTask(cb) {
+    // place code for your default task here
+    cb();
+  }
+  
+  exports.default = defaultTask
+  
+  
+  var gulp = require('gulp');
+  var sass = require('gulp-sass');
+  
+  gulp.task('sass', function(){
+      return gulp.src('scss/*.scss')
+        .pipe(sass()) // Using gulp-sass
+        .pipe(gulp.dest('css'))
+    });
+  
+    gulp.task('watch', function(){
+      gulp.watch('scss/*.scss', gulp.series(['sass'])); 
+      // Other watchers
+    })
+   
